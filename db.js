461eafad3336c22e5a2b82f43b7c24c0e4842a28
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-04 12:12:32 +0800

    Added Docker and Docker compose

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
-const {Pool} = require('pg');
-
-// Pool class to create a new pool that will connect to the database
+const { Pool } = require('pg')
 const pool = new Pool({
-host: "db",
-port: 5432,
-user: "postgres",
-password: "password",
-database: "db123"
-
+    host: 'db',
+    port: 5432,
+    user: 'user123',
+    password: 'password123',
+    database: 'db123'
 })
 
-
-module.exports = pool;
+module.exports = pool
--- server.js
+++ server.js
@@ -1,47 +1,41 @@
-const express = require("express");
-const pool = require("./db");
+const express = require('express')
+const pool = require('./db')
+const port = 3000
 
-const port = 1432;
-const app = express();
+const app = express()
+app.use(express.json())
 
-app.use(express.json());
-
-// Routes
-
-app.listen(port, () => {
-  console.info(`Server is running on port ${port}`);
-});
-
-app.get("/", async (req, res) => {
+//routes
+app.get('/', async (req, res) => {
     try {
-       const data =  await pool.query(
-          "SELECT * FROM school"
-        );
-        res.status(200).send(data.rows);
-      } catch (error) {
-        console.log(error.message);
-      }
-});
+        const data = await pool.query('SELECT * FROM schools')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 
-app.post("/", async (req, res) => {
-  const { name, location } = req.body;
-  try {
-    await pool.query(
-      "INSERT INTO school (name, address) VALUES($1, $2)", [name, location]
-    );
-    res.status(200).send("School added successfully");
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+app.post('/', async (req, res) => {
+    const { name, location } = req.body
+    try {
+        await pool.query('INSERT INTO schools (name, address) VALUES ($1, $2)', [name, location])
+        res.status(200).send({ message: "Successfully added child" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 
-app.get("/setup", async (req, res) => {
-  try {
-    await pool.query(
-      "CREATE TABLE school (id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))"
-    );
-    res.status(200).send("School added successfully");
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query('CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))')
+        res.status(200).send({ message: "Successfully created table" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`))
